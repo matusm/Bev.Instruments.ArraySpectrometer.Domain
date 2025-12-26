f801a85a61eb510b8fd29b3c7102af095450eac3
Michael Matus <michael@matus.at>
2025-12-26 10:25:50 +0100

    Add normalized intensity extension and update metadata
    Added NormalizedIntensity.cs with an extension method for IArraySpectrometer to compute normalized intensity data. Updated project file to include the new source. Changed copyright to "Michael Matus 2025" and bumped assembly version to 1.2.0.0.

---
+++ Bev.Instruments.ArraySpectrometer.Domain/NormalizedIntensity.cs
@@ -0,0 +1,15 @@
+﻿using Bev.Instruments.ArraySpectrometer.Abstractions;
+using System.Linq;
+
+namespace Bev.Instruments.ArraySpectrometer.Domain
+{
+    public static class NormalizedIntensity
+    {
+        public static double[] GetNormalizedIntensityData(this IArraySpectrometer spectrometer)
+        {
+            double saturationLevel = spectrometer.SaturationLevel; // Getting SaturationLevel might be computationally expensive -> cache the value
+            double[] intensityData = spectrometer.GetIntensityData();
+            return intensityData.Select(intensity => intensity / saturationLevel).ToArray();
+        }
+    }
+}
--- Bev.Instruments.ArraySpectrometer.Domain/Properties/AssemblyInfo.cs
+++ Bev.Instruments.ArraySpectrometer.Domain/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Bev.Instruments.ArraySpectrometer.Domain")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("Copyright © Michael Matus 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
